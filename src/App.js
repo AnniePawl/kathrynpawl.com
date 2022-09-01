e4f6da83ac271a454765b71a187f538c4a56b1dd
Anna Pawl <annampawl@gmail.com>
2022-08-31 22:32:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kathryn Pawl</h1>
+      <p>
+        ux designer. dog lover. sister. daughter. watercolorist. friend.
+        necklace maker. mac n cheese chef extraordinaire. fashionista. smarty
+        pants. overall dope af{" "}
+      </p>
     </div>
   );
 }
