ea1f0ff5159b72c22a79913c1d18834041f9d0c3
AnniePawl <annampawl@gmail.com>
2023-01-16 10:36:30 -0500

    landing page

--- src/Landing.js
+++ src/Landing.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Nav from './Nav';
 import './landing.scss';
 
@@ -8,6 +8,7 @@ export default function Landing(){
     const [theme3, setTheme3] = useState(false);
     const [theme4, setTheme4] = useState(false);
 
+  
     const handleTheme1 = () =>{
         setTheme1(true);
         setTheme2(false);
@@ -38,23 +39,22 @@ export default function Landing(){
     }
 
     return(
-       <div className={`${theme1?"theme1":""} ${theme2?"theme2":""} ${theme3?"theme3":""} ${theme4?'theme4':''} h-screen flex items-center justify-center`}>
+       <div className={`overflow-hidden${theme1?"theme1":""} ${theme2?"theme2":""} ${theme3?"theme3":""} ${theme4?'theme4':''} h-screen flex items-center justify-center`}>
             <Nav theme={`${theme1?"theme1":""}${theme2?"theme2":""}${theme3?"theme3":""}${theme4?"theme4":""}`}/>
             <div className='absolute flex top-5 right-5'>
-                <div onClick={handleTheme1} className='theme1-btn h-6 w-6 rounded-full border border-zinc-800 theme1 m-1 hover:cursor-pointer'></div>
-                <div onClick={handleTheme2} className='theme2-btn h-6 w-6 rounded-full border border-zinc-800 theme3 m-1 hover:cursor-pointer'></div>
-                <div onClick={handleTheme3} className='theme3-btn h-6 w-6 rounded-full border border-zinc-800 theme4 m-1 hover:cursor-pointer'></div>
-                <div onClick={handleTheme4} className='theme4-btn h-6 w-6 rounded-full border border-zinc-800 theme2 m-1 hover:cursor-pointer'></div>
+                <div onClick={handleTheme1} className='theme1-btn h-5 w-5 md:h-6 md:w-6 rounded-full border border-zinc-800 theme1 m-1 hover:cursor-pointer'></div>
+                <div onClick={handleTheme2} className='theme2-btn h-5 w-5 md:h-6 md:w-6 rounded-full border border-zinc-800 theme3 m-1 hover:cursor-pointer'></div>
+                <div onClick={handleTheme3} className='theme3-btn h-5 w-5 md:h-6 md:w-6 rounded-full border border-zinc-800 theme4 m-1 hover:cursor-pointer'></div>
+                <div onClick={handleTheme4} className='theme4-btn h-5 w-5 md:h-6 md:w-6 rounded-full border border-zinc-800 theme2 m-1 hover:cursor-pointer'></div>
             </div>
-            <div class="flex flex-col max-w-3xl ">
-                <h1 className={`${theme1?'text-zinc-800':''} ${theme2?'text-red-500':''} ${theme3?'text-white':''} ${theme4?'text-yellow-300':''} text-8xl mb-4`}>Kathryn Pawl</h1>
-                <p className={`${theme1?'text-slate-800':''} ${theme2?'text-red-500':''} ${theme3?'text-white':''} ${theme4?'text-yellow-300':''} text-lg`}>
-                    I’m a graphic illustrator and user interface designer with experience using industry-standard tools to create visual assets and unique digital experiences. I’m passionate about bringing ideas to life and engaging users through interactivity. Want to learn more about my services from logo packages to website prototypes? 
-                    <a href="mailto:kathrynpawl@gmail.com">Reach Out</a> 
-                </p>
-            </div>
-            <div class='absolute bottom-0 w-screen h-24'>
-
+            <div class="relative flex md:justify-center w-screen px-8">
+                <div className='flex flex-col'>
+                    <h1 className={`text-5xl md:text-7xl md:text-left mb-6 tracking-wider max-w-2xl ${theme1?'text-zinc-800':''} ${theme2?'text-red-500':''} ${theme3?'text-white':''} ${theme4?'text-yellow-300':''} text-8xl font-bold`}>Kathryn Pawl</h1>
+                    <p className={`text-base md:text-xl max-w-2xl ${theme1?'text-slate-800':''} ${theme2?'text-red-500':''} ${theme3?'text-white':''} ${theme4?'text-yellow-300':''} text-lg `}>
+                        I’m a graphic illustrator and user interface designer with experience using industry-standard tools to create visual assets and unique digital experiences. I’m passionate about bringing ideas to life and engaging users through interactivity. Want to learn more about my services from logo packages to website prototypes? 
+                        <a href="mailto:kathrynpawl@gmail.com"><span className='text-blue-500'> Reach Out</span></a> 
+                    </p>
+                </div>
             </div>
         </div>
     )
--- src/Nav.js
+++ src/Nav.js
@@ -10,15 +10,15 @@ export default function Nav(props){
     }
 
     return(
-        <div>
+        <div className='hidden'>
             {/* Hamburger Menu */}
-            <div onClick={hamburger} className='z-10 fixed top-5 left-5 hover:cursor-pointer'>
+            <div onClick={hamburger} className='z-20 fixed top-5 left-5 hover:cursor-pointer'>
                 <div className={`${active?'rotate-45 translate-y-2 !bg-zinc-800':''} ${theme=='theme1'?'bg-zinc-800':''} ${theme=='theme2'?'theme2-nav':''} ${theme=='theme3'?'theme3-nav':''} ${theme=='theme4'?'theme4-nav':''} h-1 w-6 mb-1 transition-all`}></div>
                 <div className={`${active?'hidden':''}  ${theme=='theme1'?'bg-zinc-800':''} ${theme=='theme2'?'theme2-nav':''} ${theme=='theme3'?'theme3-nav':''} ${theme=='theme4'?'theme4-nav':''} h-1 w-6 mb-1 transition-all`}></div>
                 <div className={`${active?'-rotate-45 !bg-zinc-800':''}  ${theme=='theme1'?'bg-zinc-800':''} ${theme=='theme1'?'bg-zinc-800':''} ${theme=='theme2'?'theme2-nav':''} ${theme=='theme3'?'theme3-nav':''} ${theme=='theme4'?'theme4-nav':''} h-1 w-6 mb-1 transition-all`}></div>
             </div>
             {/* Nav Slideout */}
-            <div className={`${active?'left-0':'-left-64'} ${theme=='theme1'?'theme1-nav':''} ${theme=='theme2'?'theme2-nav':''} ${theme=='theme3'?'theme3-nav':''} ${theme=='theme4'?'theme4-nav':''} h-screen w-64 bg-yellow-300 absolute top-0 transition-all`}>
+            <div className={`z-10 ${active?'left-0':'-left-64'} ${theme=='theme1'?'theme1-nav':''} ${theme=='theme2'?'theme2-nav':''} ${theme=='theme3'?'theme3-nav':''} ${theme=='theme4'?'theme4-nav':''} h-screen w-64 bg-yellow-300 absolute top-0 transition-all`}>
                 <ul className='pt-32 px-10'>
                     <li className={`text-lg text-zinc-800 font-bold mb-2`}>About</li>
                     <li className='text-lg text-zinc-800 font-bold mb-2'>Illustrations</li>
